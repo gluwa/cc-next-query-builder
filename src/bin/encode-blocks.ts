151431f3de30db0ca79ba5b5a37cec7280e7fbe4
Alex Todorov <alex.todorov@gluwa.com>
2025-12-15 12:28:19 +0200

    Don't create a new local variable, update the existing one

--- src/bin/encode-blocks.ts
+++ src/bin/encode-blocks.ts
@@ -38,7 +38,7 @@ async function encodeTransaction(
   console.log(`*** DEBUG: before encoding tx=${txHash}, transaction=${transaction!}, receipt=${receipt!}`);
   if (receipt === null) {
     // 80 credits
-    const receipt = await provider.getTransactionReceipt(txHash);
+    receipt = await provider.getTransactionReceipt(txHash);
     console.log(`*** DEBUG: after fetching receipt for tx=${txHash} explicitly, receipt=${receipt!}`);
   }
   console.log(`*** DEBUG: will encode tx=${txHash}, transaction=${transaction!}, receipt=${receipt!}`);
