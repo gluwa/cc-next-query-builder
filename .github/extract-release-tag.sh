6eccfca4202a775a197b76b6a98b360a32e84bcc
Alex Todorov <alex.todorov@gluwa.com>
2026-05-01 12:36:55 +0300

    Update repository location of gluwa/creditcoin3

--- .github/extract-release-tag.sh
+++ .github/extract-release-tag.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 ### find the latest release for testnet or mainnet
-OWNER_REPO_SLUG="gluwa/creditcoin3-next"
+OWNER_REPO_SLUG="gluwa/creditcoin3"
 GREP_FOR="$1"
 RELEASE_TAG=$(curl --silent --header "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER_REPO_SLUG/releases" | jq -r ".[].tag_name" | grep "$GREP_FOR" | head -n1)
 
